a477e4b261585b1550e28ea35c04de07eea16118
Iciar Esteve <iciarisabel.estevearenaza@soprasteria.com>
2018-04-11 13:35:16 +0200

    style replace unuseful id for class tag

--- utils.js
+++ utils.js
@@ -1,14 +1,13 @@
 window.onload = () => {
   var content = document.getElementsByClassName("content");
   var nav = document.getElementsByClassName("navigation");
-  document.getElementById("menu-button").addEventListener("click",() => {
+  document.getElementsByClassName("menu-button-header")[0].addEventListener("click",() => {
     if(window.innerWidth <= 768){
       nav[0].classList.toggle("navigation-visible");
       content[0].classList.toggle("content-blurred");
     }
   });
   content[0].addEventListener("click",() => {
-    console.log("se ha pulsado el contenido");
     if(window.innerWidth <= 768 && nav[0].classList.contains("navigation-visible") && content[0].classList.contains("content-blurred")){
       nav[0].classList.remove("navigation-visible");
       content[0].classList.remove("content-blurred");
