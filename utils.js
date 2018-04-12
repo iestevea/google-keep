9907b626d96d3168e61fbb60950d0bddab697018
Iciar Esteve <iciarisabel.estevearenaza@soprasteria.com>
2018-04-12 12:21:53 +0200

    feat add eventListener to add note button

--- utils.js
+++ utils.js
@@ -3,22 +3,28 @@ function sideMenuBehavior() {
   var content = document.getElementsByClassName("content")[0];
   var nav = document.getElementsByClassName("navigation")[0];
   var menuBtn = document.getElementsByClassName("menu-button-header")[0];
+  var addNoteBtn = document.getElementsByClassName("addNoteBtn")[0];
 
   menuBtn.addEventListener("click", showMenu);
   content.addEventListener("click", hideMenu);
-
+  addNoteBtn.addEventListener("click", addNote);
+  
   function showMenu() {
     if(window.innerWidth <= 768){
       nav.classList.toggle("navigation-visible");
       content.classList.toggle("content-blurred");
     }
   }
-
+  
   function hideMenu() { 
     if(window.innerWidth <= 768){ // && nav.classList.contains("navigation-visible") && content.classList.contains("content-blurred") -- Note: Removing a class that does not exist, does NOT throw an error!!!
-      nav.classList.remove("navigation-visible");
-      content.classList.remove("content-blurred");
-    }
+    nav.classList.remove("navigation-visible");
+    content.classList.remove("content-blurred");
+  }
+}
+
+  function addNote(){
+    console.log("se ha pulsado el btn añadir nota");
   }
 
 }
