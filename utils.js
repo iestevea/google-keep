d6aeabd3e78642e4a9283d5c57f8740e72cb2aa7
Iciar Esteve <iciarisabel.estevearenaza@soprasteria.com>
2018-04-13 13:31:02 +0200

    refactor: code refactor

--- utils.js
+++ utils.js
@@ -1,15 +1,15 @@
+function initMethods() {
+  sideMenuBehavior();
+  manageNotes();
+}
+
 function sideMenuBehavior() {
-  
   var content = document.getElementsByClassName("content")[0];
   var nav = document.getElementsByClassName("navigation")[0];
   var menuBtn = document.getElementsByClassName("menu-button-header")[0];
-  var addNoteBtn = document.getElementsByClassName("addNoteBtn")[0];
-  var inputNote = document.getElementsByClassName("searcher-content")[0].getElementsByTagName("input")[0];
 
   menuBtn.addEventListener("click", showMenu);
   content.addEventListener("click", hideMenu);
-  addNoteBtn.addEventListener("click", addNote);
-  inputNote.addEventListener("keyup", enableAddBtn);
 
   function showMenu() {
     if(window.innerWidth <= 768){
@@ -21,18 +21,27 @@ function sideMenuBehavior() {
   function hideMenu() { 
       nav.classList.remove("navigation-visible");
       content.classList.remove("content-blurred");
+  }
 }
+
+function manageNotes() {
+
+  var content = document.getElementsByClassName("content-notes")[0];
+  var addNoteBtn = document.getElementsByClassName("addNoteBtn")[0];
+  var inputNote = document.getElementsByClassName("searcher-content")[0].getElementsByTagName("input")[0];
+  addNoteBtn.addEventListener("click", addNote);
+  inputNote.addEventListener("keyup", enableAddBtn);
 
   function addNote() {
     var textNote = inputNote.value;
     var newNote = document.createElement("div");
-    newNote.setAttribute("class","content-notes");
+    newNote.setAttribute("class","content-notes-note");
     newNote.innerHTML = "<p>"+textNote+"</p>";
     content.appendChild(newNote);
     inputNote.value = '';
     enableAddBtn();
   }
-
+  
   function enableAddBtn() {
     if(inputNote.value != ''){
       addNoteBtn.classList.remove("disabled");
@@ -40,5 +49,4 @@ function sideMenuBehavior() {
       addNoteBtn.classList.add("disabled");
     }
   }
-
 }
